c223512f0a551c6042d8109049316ec5b07c4e4a
Rene Cruz <renecruz.0711@gmail.com>
2024-03-08 01:56:08 -0600

    Solved exercise

---
+++ codewars/javascript/4_parse_int_from_char/script.js
@@ -0,0 +1,7 @@
+// ! Parsing int from char
+
+function getAge (inputString) {
+  // ? There does exist a function in javascript for that
+  // ? parseInt is made to convert from a data type to Integer
+  return parseInt(inputString);
+}
