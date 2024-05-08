02da970b4ccd454430f316b53fd7aacbe614106d
Rene Cruz <renecruz.0711@gmail.com>
2024-05-08 00:22:28 -0600

    Adding solution #1

--- codesignal/10_matrixElementsSum/src/App.java
+++ codesignal/10_matrixElementsSum/src/App.java
@@ -1,6 +1,35 @@
 public class App {
+
+    public static int matrixElementSum (int[][] matrix ) {
+        // * Declaring the sum as zero (initially)
+        int total = 0;
+        // ? iterate the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                // ? Checking if it is the first array
+                if (i == 0) {
+                    // ! Checking if the element is different than zero
+                    if (matrix[i][j] > 0) {
+                        total += matrix[i][j];
+                    }
+                }else {
+                    // ! Checking if the element is different than zero
+                    if (matrix[i][j] > 0) {
+                        // ! Checking if the element above is not zero 
+                        if (matrix[i - 1][j] > 0) {
+                            total += matrix[i][j];
+                        }
+                    }
+                }
+            }
+        }
+        return total;
+    }
+
     public static void main(String[] args) throws Exception {
         // ? Adding the initial configuration to my solution
-        
+        int[][] matrix = {{0, 1, 1, 2}, {0, 5, 0, 0}, {2, 0, 3, 3}};
+
+        System.out.println(matrixElementSum(matrix));
     }
 }
