e2f1d4b7dd181d43086915fc70558698fdfd8ba9
Rene Cruz <renecruz.0711@gmail.com>
2024-05-09 16:07:56 -0600

    Exercise 11 - SOLVED!

--- codesignal/11_all_longest_strings/src/App.java
+++ codesignal/11_all_longest_strings/src/App.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
+
+
+    int findLargestLength (String[] strings) {
+        int aux = strings[0].length();
+        for (String element : strings) {
+            if (element.length() > aux) {
+                aux = element.length();
+            }
+        }
+        return aux;
+    }
+
+    String[] findStringsInArray (String[] inputArray) {
+        List<String> largestStrings = new ArrayList<>();
+        for (String element : inputArray) {
+            if (findLargestLength(inputArray) == element.length()) {
+                largestStrings.add(element);
+            }
+        }
+        return largestStrings.toArray(new String[0]);
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
