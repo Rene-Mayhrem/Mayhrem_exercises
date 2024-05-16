5b206493c1758b0f0a772dfa195ca9fbbf20295d
mayhrem <renecruz.0711@gmail.com>
2024-05-15 23:15:24 -0600

    Exercise #7 resolved

--- codewars/javascript/7_printing_array_elements_with_commma_delimeters/index.js
+++ codewars/javascript/7_printing_array_elements_with_commma_delimeters/index.js
@@ -1,3 +1,6 @@
 function printArray (array) {
-  
+  // #? join is for adding the elements of an array with a delimeter as parameter
+  return array.join(",");
 }
+
+console.log(printArray(["hola", "como", "estas"]));
