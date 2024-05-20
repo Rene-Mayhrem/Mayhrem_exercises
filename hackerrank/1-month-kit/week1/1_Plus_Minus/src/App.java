12dd28ab50264744db15792445e6bb743d34cb3c
mayhrem <renecruz.0711@gmail.com>
2024-05-19 19:01:59 -0600

    Adding solution #1

--- hackerrank/1-month-kit/week1/1_Plus_Minus/src/App.java
+++ hackerrank/1-month-kit/week1/1_Plus_Minus/src/App.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
+
+
+    public static void plusMinus (List<Integer> arr) {
+        int positive = 0, negative = 0, zeros = 0;
+
+        for(int number: arr) {
+            if(number > 0) {
+                positive++;
+            } else if (number < 0) {
+                negative++;
+            } else {
+                zeros++;
+            }
+        }
+        System.out.println(String.format("%.6f", positive));
+        System.out.println(String.format("%.6f", negative));
+        System.out.println(String.format("%.6f", zeros));
+    }
+
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        List<Integer> arr = new ArrayList<>();
+
+        arr.add(1);
+        arr.add(1);
+        arr.add(0);
+        arr.add(-1);
+        arr.add(-1);
+        
+        plusMinus(arr);
     }
 }
