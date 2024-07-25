4bf23df582a5c945e254c343d589a48b82a05b9e
Rene Cruz <renecruz.0711@gmail.com>
2024-07-24 20:44:57 -0600

    Adding new solution

--- hackerrank/1-month-kit/week1/Min_Max_Sum/src/App.java
+++ hackerrank/1-month-kit/week1/Min_Max_Sum/src/App.java
@@ -1,9 +1,25 @@
-public class App {
+import java.util.List;
 
-    
+public class App {
 
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+}
+
+
+class Solution {
+    
+    public static void miniMaxSum (List<Integer> arr) {
+
+    }
+
+    public void sortMinToMax (List<Integer> arr) {
+
+    }
+
+    public void swapElements (List<Integer> arr, Integer firstElement, Integer secondElement) {
+
     }
 }
