f8f697b74370735eac52c2a84d4d71a79dfbe5e8
Rene Cruz <renecruz.0711@gmail.com>
2024-08-05 16:15:22 -0600

    Adding changes

--- leetcode/1-two-sum/src/Maps.java
+++ leetcode/1-two-sum/src/Maps.java
@@ -5,26 +5,45 @@ public class Maps {
   
   public static void main(String[] args) {
 
-    Solution solution = new Solution();
+    MapSolution solution = new MapSolution();
 
     int[] numbers = {2, 7, 11, 15};
-
-    System.out.println("Test");
-
-    solution.twoSum(numbers, 9);
+    int[] values = solution.twoSum(numbers, 9);
+    for(int value : values) {
+      System.out.println(value);
+    }
     
   }
 }
 
-class Solution {
-  public int[] twoSum (int[] numbers, int target) {
-    System.out.println("Hello");
-    HashMap<Integer, Integer> numbersMap = new HashMap<>(); 
+class MapSolution {
+
+  private HashMap<Integer, Integer> arrayToHashMap (int[] numbers) {
+    HashMap<Integer, Integer> numbersMap = new HashMap<>();
+    // ? Creating the hash map, where the key is thwe index and the value is the element in the selected index.
     for (int i = 0; i < numbers.length; i++) {
       numbersMap.put(i, numbers[i]);
-      System.out.println("Print");
     }
-    System.out.println(numbersMap);
-    return null;
+    return numbersMap;
   }
+
+  public int[] twoSum (int[] numbers, int target) {
+    HashMap<Integer, Integer> numbersMap = this.arrayToHashMap(numbers);
+    int[] values = new int[2];
+    System.out.println(numbersMap);
+    numbersMap.forEach((key, value) -> {
+      if(value <= target) {
+        Integer aux = Math.abs(target - value);
+        if (numbersMap.containsValue(aux)) {
+          values[0] = key;
+          values[1] = numbersMap.getOrDefault(key, aux);
+        }
+      }
+
+    });
+
+    
+    return values;
+  }
+
 }
