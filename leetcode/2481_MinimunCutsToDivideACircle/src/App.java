dffbeb4d6343aa7e609fbef2d07c180102dce0fe
Rene Cruz <renecruz.0711@gmail.com>
2024-08-07 22:28:26 -0600

    Cleaning code

--- leetcode/2481_MinimunCutsToDivideACircle/src/App.java
+++ leetcode/2481_MinimunCutsToDivideACircle/src/App.java
@@ -6,11 +6,8 @@ public class App {
 
 class Solution {
     public int numberOfCuts (int n) {
-        if (n == 1) {
-            return 0;
-        } else if (n % 2 == 0) {
-            return n / 2;
-        }
-        return n;
+        // ? not cutting
+        if (n == 1) return 0;
+        return (n % 2 == 0) ? n / 2 : n;
     }
 }
