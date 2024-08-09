755213c0f573b4f1d7bfd6c1b63953bbafe932b1
mayhrem <renecruz.0711@gmail.com>
2024-08-08 23:05:09 -0600

    Improved version

--- leetcode/3146_PermutationDifferenceBetweenTwoStrings/src/App.java
+++ leetcode/3146_PermutationDifferenceBetweenTwoStrings/src/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Solution sol = new Solution();
         System.out.println(sol.findPermutationDifference("abc", "bac"));
+        System.out.println(sol.findPermutationDifferenceBetter("abc", "bac"));
     }
 }
 
@@ -13,7 +14,6 @@ class Solution {
     public HashMap<String, Integer> stringToMap (String string) {
         HashMap<String, Integer> map = new HashMap<>();
         char[] arrayString = string.toCharArray();
-
         for (int i = 0; i < string.length(); i++) {
             map.put(String.valueOf(arrayString[i]) , i);
         }
@@ -26,6 +26,15 @@ class Solution {
         int permutationDifference = 0;
         for (char character : s.toCharArray()) {
             permutationDifference += Math.abs(mapS.get(String.valueOf(character)) - mapT.get(String.valueOf(character)));
+        }
+        return permutationDifference;
+    }
+
+    public int findPermutationDifferenceBetter (String s, String t) {
+        HashMap<String, Integer> map = stringToMap(s);
+        int permutationDifference = 0;
+        for (int i = 0; i < t.length(); i++) {
+            permutationDifference += Math.abs(map.get(String.valueOf(t.charAt(i))) - i);
         }
         return permutationDifference;
     }
