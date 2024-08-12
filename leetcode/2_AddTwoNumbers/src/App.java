75ad99e536564e1720ff3d91bfb68535eaab57ab
mayhrem <renecruz.0711@gmail.com>
2024-08-11 23:15:57 -0600

    Adding another solution to the problem

--- leetcode/2_AddTwoNumbers/src/App.java
+++ leetcode/2_AddTwoNumbers/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Solution solution = new Solution();
+        solution.addTwoNumbers(new LinkedList<>(Arrays.asList(1, 2, 3)), new LinkedList<>(Arrays.asList(4, 5, 6)));
+    }
+}
+
+
+class Solution {
+    public LinkedList addTwoNumbers (LinkedList l1, LinkedList l2) {
+        //? reverse my lists
+        l1.RE
+        return null;
     }
 }
