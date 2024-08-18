efaf0d86ca8df36048da7d9d1d31ea8c825ca8c7
Rene Cruz <renecruz.0711@gmail.com>
2024-08-17 21:20:46 -0600

    Creating initial challenges

--- hackerrank/W2_LonelyInteger/src/App.java
+++ hackerrank/W2_LonelyInteger/src/App.java
@@ -16,9 +16,11 @@ class Solution {
     public static int lonelyInteger (List<Integer> a) {
         HashMap<Integer, Integer> integerMap = new HashMap<>();
         for (int number : a) {
+            //? Creating a hashMap by number and the times of occurence
             integerMap.put(number, integerMap.getOrDefault(number, 0) + 1);
         }
         for (Map.Entry<Integer, Integer> entry : integerMap.entrySet()) {
+            //! There is the constraint that says that always is going to exist one number with one occurence
             if (entry.getValue() == 1) {
                 return entry.getKey();
             }
