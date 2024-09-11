c3abf997c4ac51373bc9dbb204571fc06eeb5cc8
mayhrem <renecruz.0711@gmail.com>
2024-09-11 13:01:32 -0600

    Adding min max sum exercise

---
+++ hackerrank/W1_Min_Max_Sum/src/App.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+class Sorting {
+    private List list;
+
+    public List getList() {
+        return list;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+
+    public Sorting (List list) {
+        this.list = list;
+    }
+
+    
+}
