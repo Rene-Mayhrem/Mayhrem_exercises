5a423f0bd852af523e8b2172c79f8b94f76933e7
mayhrem <renecruz.0711@gmail.com>
2024-09-22 18:54:03 -0600

    Adding solution

--- leetcode/83_RemoveDuplicatesFromSortedList/src/App.java
+++ leetcode/83_RemoveDuplicatesFromSortedList/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        ListNode head = new ListNode();
+        head.val = 1;
+        head.append(1);
+        head.append(2);
+        head.append(3);
+        head.append(3);
+
+        head.print();
+        head.deleteDuplicates(head);
+        System.out.println("Deleted duplicated");
+        head.print();
     }
 }
--- leetcode/83_RemoveDuplicatesFromSortedList/src/ListNode.java
+++ leetcode/83_RemoveDuplicatesFromSortedList/src/ListNode.java
@@ -1,3 +1,45 @@
 public class ListNode {
-  i
+  int val;
+  ListNode next;
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode() {}
+
+  public void append (int val) {
+    ListNode newNode = new ListNode(val);
+    if (this.next == null) {
+      this.next = newNode;
+    } else {
+      ListNode current = this.next;
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  public void print() {
+    ListNode current = this;
+    while (current != null) {
+      System.out.println(current.val);
+      current = current.next;
+    }
+  }
+
+  public ListNode deleteDuplicates (ListNode head) {
+    while(head.next != null) {
+      ListNode temp = head.next;
+      if (head.val == temp.val) {
+        head.next = temp.next;
+        temp = null;
+      } else {
+        head = head.next;
+      }
+    }
+    return head;
+  }
+
 }
