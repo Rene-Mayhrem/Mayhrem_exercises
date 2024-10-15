0a5b606969bd49dad83c99b6150de979e5a2b649
Rene Cruz <renecruz.0711@gmail.com>
2024-10-15 12:46:42 -0600

    Adding another possible solution for the code

--- leetcode/322_CoinChange/src/App.java
+++ leetcode/322_CoinChange/src/App.java
@@ -6,19 +6,26 @@ import java.util.Collections;
 public class App {
     public static void main(String[] args) throws Exception {
         Solution solution = new Solution();
-        int[] coins = {6, 1, 2, 5};
+        int[] coins = {186, 419, 83, 408};
         
-        System.out.println(solution.coinChange(coins, 11));
+        System.out.println(solution.coinChange(coins, 6249));
     }
 }
 
 class Solution {
     public int coinChange (int[] coins, int amount) {
         
+        bubbleSort(coins);
+
+        for (int coin : coins) {
+            System.out.println(coin);
+        }
 
         int ans = 0;
         for (int i = coins.length - 1; i >= 0; i--) {
+            System.out.println(coins[i]+" VS "+amount);
             while (coins[i] <= amount) {
+                System.out.println("Heey");
                 ans++;
                 amount = amount - coins[i];
             }
@@ -27,24 +34,20 @@ class Solution {
         return ans >= 0 && amount == 0 ? ans : -1;
     }
 
-    private void quicksort (int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-
-
-        }
-    }
-
-    private int partition (int[] arr, int low, int high) {
-        int pivot = arr[high];
-
-        int i = (low - 1);
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+    private static void bubbleSort (int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
             }
+            if (!swapped) break;
         }
     }
 }
