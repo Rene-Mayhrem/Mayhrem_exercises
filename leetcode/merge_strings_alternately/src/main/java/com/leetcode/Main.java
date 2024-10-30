ae97ca2aa2b98cba104e904a612f2a21bfba2e96
Rene Cruz <renecruz.0711@gmail.com>
2024-10-29 21:45:53 -0600

    Adding simple test case

--- leetcode/merge_strings_alternately/src/main/java/com/leetcode/Main.java
+++ leetcode/merge_strings_alternately/src/main/java/com/leetcode/Main.java
@@ -3,5 +3,7 @@ package com.leetcode;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Solution solution = new Solution();
+        System.out.println(solution.mergeAlternately("ab", "pqrs"));
     }
 }
