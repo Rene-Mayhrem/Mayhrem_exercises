818a5a222e74b59621d4bc175ed080691be40251
mayhrem <renecruz.0711@gmail.com>
2024-10-31 22:09:54 -0600

    Adding new project

--- leetcode/1133_LargestUniqueNumber/src/App.java
+++ leetcode/1133_LargestUniqueNumber/src/App.java
@@ -2,8 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int[] nums = {5, 7, 3, 9, 4, 9, 8, 3, 1};
-        
+
         Solution solution = new Solution();
-        solution.largestUniqueNumber(nums);
+        System.out.println(solution.largestUniqueNumber(nums));
     }
 }
--- leetcode/1133_LargestUniqueNumber/src/Solution.java
+++ leetcode/1133_LargestUniqueNumber/src/Solution.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class Solution {
   public int largestUniqueNumber (int[] nums) {
@@ -10,7 +9,7 @@ public class Solution {
     for (int num : nums) {
       frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
     }
-    for (int i = nums.length - 1; i > 0; i--) {
+    for (int i = nums.length - 1; i >= 0; i--) {
       if (frequencyMap.get(nums[i]) == 1) {
         return nums[i];
       }
