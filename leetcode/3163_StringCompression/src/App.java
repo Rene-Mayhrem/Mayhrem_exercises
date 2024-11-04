4ef360f321250e05c2df11f17762f9e3e7804158
mayhrem <renecruz.0711@gmail.com>
2024-11-03 22:04:26 -0600

    Solved

---
+++ leetcode/3163_StringCompression/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // String word = "abcde";
+        String word = "aaaaaaaaaaaaaabb";
+        System.out.println(new Solution().compressedString(word));
+    }
+}
