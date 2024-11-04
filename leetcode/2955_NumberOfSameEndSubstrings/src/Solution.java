acdd612a18959ac293ebafbc2d728ced0a463054
mayhrem <renecruz.0711@gmail.com>
2024-11-03 22:03:58 -0600

    Solved

---
+++ leetcode/2955_NumberOfSameEndSubstrings/src/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+  public int[] sameEndSubstringCount (String s, int[][] queries) {
+    /*
+     * input -> String s, int[][] queries
+     * String s -> 0-indexed string
+     * int[][] queries -> where queries[i] represents a substring of s starting from the index [li, ri]
+     * 
+    */
+    /*
+     * Output -> return array of integers
+     * ans -> where ans[i] is the number of same-end substrings of queries[i]
+     * 
+     */
+    return null;
+  }
+}
