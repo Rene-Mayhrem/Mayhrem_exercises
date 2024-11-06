fa109824138c081d62a0a6b317cc4d9c4bc1479b
mayhrem <renecruz.0711@gmail.com>
2024-11-05 20:08:21 -0600

    Solved

---
+++ leetcode/2914_MinimumNumberChangesToMakeBinaryStringBeautiful/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println(new Solution().minChanges("010011001010"));
+    }
+}
