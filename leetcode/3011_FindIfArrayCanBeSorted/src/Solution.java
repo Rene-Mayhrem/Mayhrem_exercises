d9d09249ed9ea16369a2e017ca67f184fa3628af
mayhrem <renecruz.0711@gmail.com>
2024-11-06 15:43:44 -0600

    Adding solution

--- leetcode/3011_FindIfArrayCanBeSorted/src/Solution.java
+++ leetcode/3011_FindIfArrayCanBeSorted/src/Solution.java
@@ -2,24 +2,38 @@ import java.util.Arrays;
 
 public class Solution {
   public boolean canSortArray (int[] nums) {
-    int[] values = Arrays.copyOf(nums, nums.length);
-    int n = values.length;
-
+    int values[] = Arrays.copyOf(nums, nums.length);
+    int n = nums.length;
     for (int i = 0; i < n - 1; i++) {
-      for (int j = 0; j < n - i - 1; j++) {
-        if (values[j] <= values[j + 1]) {
-          continue;
+      if (values[i] <= values[i + 1]) {
+        continue;
+      } else {
+        if (
+          Integer.bitCount(values[i]) ==
+          Integer.bitCount(values[i + 1])
+        ) {
+          int temp = values[i];
+          values[i] = values[i + 1];
+          values[i + 1] = temp;
         } else {
-          if (
-            Integer.bitCount(j) ==
-            Integer.bitCount(j + 1)
-          ) {
-            int temp = values[j];
-            values[j] = values[j + 1];
-            values[j + 1] = temp;
-          } else {
-            return false;
-          }
+          return false;
+        }
+      }
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+      if (values[i] >= values[i - 1]) {
+        continue;
+      } else {
+        if (
+          Integer.bitCount(values[i]) ==
+          Integer.bitCount(values[i - 1])
+        ) {
+          int temp = values[i];
+          values[i] = values[i - 1];
+          values[i - 1] = temp;
+        } else {
+          return false;
         }
       }
     }
