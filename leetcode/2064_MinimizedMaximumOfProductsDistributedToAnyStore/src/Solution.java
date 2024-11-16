8279e3b89fe28b9088b8828919dd7527fba1f262
Rene Cruz <renecruz.0711@gmail.com>
2024-11-16 17:18:20 -0600

    Adding project

---
+++ leetcode/2064_MinimizedMaximumOfProductsDistributedToAnyStore/src/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+  public int minimizedMaximum (int n, int[] quantities) {
+    /* * 
+     * n -> number of specialty retail stores
+     * quantities[i] -> number of products 
+     * return -> the maximum number minimum possible of 
+    */
+    return 0;
+  }
+}
