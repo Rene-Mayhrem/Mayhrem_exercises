6f4868de422701990efe15b4cd9e870481aeafbd
mayhrem <renecruz.0711@gmail.com>
2024-11-21 01:50:44 -0600

    Solved

---
+++ leetcode/amazon/dynammic-programming/CoinChange/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        int[] coins = {1, 2, 5};
+        System.out.println(new Solution().coinChange(coins, 11));
+    }
+}
