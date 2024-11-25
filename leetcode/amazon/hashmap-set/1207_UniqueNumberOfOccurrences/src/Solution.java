02043ac7166db44b06bad28567f31c50dec0a13b
mayhrem <renecruz.0711@gmail.com>
2024-11-25 00:05:55 -0600

    Solved

--- leetcode/amazon/hashmap-set/1207_UniqueNumberOfOccurrences/src/Solution.java
+++ leetcode/amazon/hashmap-set/1207_UniqueNumberOfOccurrences/src/Solution.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Solution {
 
@@ -7,14 +8,22 @@ public class Solution {
   //? Iterate through the arr 
     //? Insert the element and increment the time it appears
   //? Create a hashset to store the times it apears
+  //? Iterate the value collection
+    //? Check if the element does exist -> return false
+  //? 
    
   public boolean uniqueNumberOfOccurences (int[] arr) {
     HashMap<Integer, Integer> map = new HashMap<>();
+    HashSet<Integer> set = new HashSet<>();
     for (int number : arr) {
-      map.put(number, map.getOrDefault(number, +1));
+      map.put(number, map.getOrDefault(number, 0) + 1);
     }
     System.out.println(map);
-    return false;
+    for (Integer value : map.values()) {
+      if (set.contains(value)) return false;
+      set.add(value);
+    }
+    return true;
   }
 
 }
