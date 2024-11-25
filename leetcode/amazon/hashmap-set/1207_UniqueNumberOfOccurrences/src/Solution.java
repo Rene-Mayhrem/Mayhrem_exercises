a2400adff69a72ce29c19a5b2b86210ecf96be02
mayhrem <renecruz.0711@gmail.com>
2024-11-24 23:32:44 -0600

    Creating project

--- leetcode/amazon/hashmap-set/1207_UniqueNumberOfOccurrences/src/Solution.java
+++ leetcode/amazon/hashmap-set/1207_UniqueNumberOfOccurrences/src/Solution.java
@@ -1,3 +1,20 @@
+import java.util.HashMap;
+
 public class Solution {
-  
+
+  //? Create a hashmap to check number of occurences -> key - value (arr number , times that appears)
+  //? Create a hashmap
+  //? Iterate through the arr 
+    //? Insert the element and increment the time it appears
+  //? Create a hashset to store the times it apears
+   
+  public boolean uniqueNumberOfOccurences (int[] arr) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int number : arr) {
+      map.put(number, map.getOrDefault(number, +1));
+    }
+    System.out.println(map);
+    return false;
+  }
+
 }
