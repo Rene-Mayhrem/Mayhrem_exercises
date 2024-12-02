1afc7d1429d00bf92d89c3d77fd9c4f9eee662a0
mayhrem <renecruz.0711@gmail.com>
2024-12-01 23:20:17 -0600

    Solved

--- leetcode/normal/1455_CheckIfAWordOccursAsAPrefixOfAnyWordInASentence/src/Solution.java
+++ leetcode/normal/1455_CheckIfAWordOccursAsAPrefixOfAnyWordInASentence/src/Solution.java
@@ -2,7 +2,7 @@ public class Solution {
   public int isPrefixOfWord (String sentence, String searchWord) {
     String[] words = sentence.split(" ");
     for (int i = 0; i < words.length; i++) {
-      if (words[i].contains(searchWord)) return i + 1;
+      if (words[i].startsWith(searchWord)) return i + 1;
     }
     return -1;
   }
