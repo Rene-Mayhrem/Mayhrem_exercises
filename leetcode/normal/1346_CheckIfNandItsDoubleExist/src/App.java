ba0415d111ad80f4fd57d59a3077d91ab1b6a9cf
mayhrem <renecruz.0711@gmail.com>
2024-12-01 22:42:23 -0600

    Adding project and solved

---
+++ leetcode/normal/1346_CheckIfNandItsDoubleExist/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        int[] arr = {10, 2, 5, 3};
+        System.out.println(new Solution().checkIfExist(arr));
+    }
+}
