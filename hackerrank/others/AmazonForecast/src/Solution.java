a5802301913ea9b130e633b859116bbaf7af6068
Rene Cruz <renecruz.0711@gmail.com>
2024-12-02 17:20:37 -0600

    Solved

---
+++ hackerrank/others/AmazonForecast/src/Solution.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+  public List<Integer> predictDays (List<Integer> days, int k) {
+    List<Integer> idealDays = new ArrayList<>();
+
+    
+
+  }
+}
