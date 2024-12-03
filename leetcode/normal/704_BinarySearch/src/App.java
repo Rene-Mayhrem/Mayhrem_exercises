ca8ccb08ca02b688eb9a73aa458368f2f5f69905
mayhrem <renecruz.0711@gmail.com>
2024-12-02 22:46:37 -0600

    Solved

--- leetcode/normal/704_BinarySearch/src/App.java
+++ leetcode/normal/704_BinarySearch/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        int[] nums = {5};
+        System.out.println(new Solution().search(nums, 5));
     }
 }
