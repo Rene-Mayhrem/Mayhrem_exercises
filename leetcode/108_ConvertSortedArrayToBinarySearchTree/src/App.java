9c72f647c04edc75f94af7b792be179f316af5b8
Rene Cruz <renecruz.0711@gmail.com>
2024-12-03 19:33:02 -0600

    Solved

--- leetcode/108_ConvertSortedArrayToBinarySearchTree/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        int[] nums = {-10, -3, 0, 5, 9};
-        Solution solution = new Solution();
-        TreeNode root = solution.sortedrrayToBST(nums);
-        solution.inorder(root);
-    }
-}
