44e60d4e531bd3b123ac3f96b5a9eb8f055fe1ab
Rene Cruz <renecruz.0711@gmail.com>
2024-12-03 20:41:29 -0600

    Solved

--- leetcode/amazon-spring/1492_TheKthFactorOfN/src/Solution.java
+++ leetcode/amazon-spring/1492_TheKthFactorOfN/src/Solution.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
@@ -8,9 +9,13 @@ public class Solution {
     //? Ask if number i is factor of n -> add number
     // ? Ask if factor count is equal to k -> return factor count index i
     List<Integer> factors = new ArrayList<>();
-    for (int i = 1; i <= n; i++) {
-      if (n % i == 0) factors.add(i);
+    for (int i = 1; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) { 
+        factors.add(i);
+        if (i != (n / i)) factors.add(n / i);
+      }
     }
+    Collections.sort(factors);
     return (k <= factors.size()) ? factors.get(k - 1) : - 1;
   }
 }
