7f53708a67b5e61bdfe25e5c9784a8e46e5d232e
mayhrem <renecruz.0711@gmail.com>
2024-12-09 09:05:03 -0600

    Solved

--- leetcode/normal/111_MinimumDepthOfBinaryTree/src/Pair.java
+++ leetcode/normal/111_MinimumDepthOfBinaryTree/src/Pair.java
@@ -1,3 +1,26 @@
 public class Pair {
+  
+  private TreeNode node;
+  private int level;
 
+  public Pair (TreeNode node, int level) {
+    this.node = node;
+    this.level = level;
+  }
+
+  public TreeNode getNode() {
+    return node;
+  }
+
+  public void setNode(TreeNode node) {
+    this.node = node;
+  }
+
+  public int getLevel() {
+    return level;
+  }
+
+  public void setLevel(int level) {
+    this.level = level;
+  }
 }
--- leetcode/normal/111_MinimumDepthOfBinaryTree/src/Solution.java
+++ leetcode/normal/111_MinimumDepthOfBinaryTree/src/Solution.java
@@ -6,13 +6,31 @@ import javax.swing.Painter;
 public class Solution {
 
   
+  //? 1. Make sure the tree is not empty
+  //? 2. Create a Queue to store nodes and level (depth)
+  //? -> Create a class which will be named: Pair (TreeNode, int)
+  //? Iterate my queue while is not empty
+  //? Create current pair -> node and depth
+  //? Check if node.left and node.right are empty -> return depth
+  //? Check if node.left is not null -> Add new pair with node.left and depth + 1
+  //? Check if node.right is not null -> Add new pair with node.right and depth + 1
+  
 
   public int minDepth (TreeNode root) {
     if (root == null) return 0;
-    Queue queue = new LinkedList<>({1});
+    Queue<Pair> queue = new LinkedList<>();
+    queue.add(new Pair(root, 1)); //? Initialize witg root and level 1
+    while (!queue.isEmpty()) {
+      Pair current = queue.poll();
+      TreeNode node = current.getNode();
+      int level = current.getLevel();
 
+      if (node.left == null && node.rigth == null) return level;
+      if (node.left != null) queue.add(new Pair(node.left, level + 1));
+      if (node.rigth != null) queue.add(new Pair(node.rigth, level + 1));
+    } 
+    return 0;
   }
-
-
-
 }
+
+
