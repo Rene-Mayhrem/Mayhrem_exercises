793e9b68bf93ae39d138e11493ecd5f7b3cec42c
mayhrem <renecruz.0711@gmail.com>
2024-12-09 23:50:54 -0600

    Adding project

--- leetcode/binary-search/704_BinarySearch/src/Solution.java
+++ leetcode/binary-search/704_BinarySearch/src/Solution.java
@@ -13,4 +13,38 @@ public class Solution {
     }
     return -1;
   }
+
+  public int searchTry (int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while(left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] < target) {
+        left = mid + 1;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else {
+        return mid;
+      }
+    }
+    return -1;
+  }
+
+  public int searchRecursive (int[] nums, int target) {
+    return binarySearch(nums, 0, nums.length - 1, target);
+  }
+
+  public int binarySearch (int[] nums, int left, int right, int target) {
+    if (left > right) {
+      return -1;
+    }
+    int mid = left + (right - left) / 2;
+    if (nums[mid] == target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      return binarySearch(nums, mid + 1, right, target);
+    } else {
+      return binarySearch(nums, left, mid - 1, target);
+    }
+  }
 }
