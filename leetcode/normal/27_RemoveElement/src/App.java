fae9e32ff13efcc1d0059e7173ce3239aaaa451a
Rene Cruz <renecruz.0711@gmail.com>
2024-12-16 16:10:26 -0600

    Adding solution

---
+++ leetcode/normal/27_RemoveElement/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(new Solution().removeElementSolution(nums, 2));
+    }
+}
