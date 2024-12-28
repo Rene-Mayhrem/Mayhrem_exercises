d24a7c47c25d09d62de420226663febb8a89ecfa
mayhrem <renecruz.0711@gmail.com>
2024-12-28 04:13:38 -0600

    Adding project

---
+++ leetcode/leetcode-75/prefix-sum/1732_FindTheHighestAltitude/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        int[] gains = {-5, 1, 5, 0, -7};
+        Solution solution = new Solution();
+        System.out.println(solution.largestAltitude(gains));
+    }
+}
