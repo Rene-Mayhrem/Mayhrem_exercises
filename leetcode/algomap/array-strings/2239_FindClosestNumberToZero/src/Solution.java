279900de739d7f6fcc2ab6618b9d012c4f3e97d8
mayhrem <renecruz.0711@gmail.com>
2024-12-28 22:32:43 -0600

    Adding first project

---
+++ leetcode/algomap/array-strings/2239_FindClosestNumberToZero/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+  // ? Create a variable to store the max index
+  //?
+  // ? Iterate over the array finding the absolute difference with current number with zero
+
+  public int findClosestNumber (int[] nums) {
+
+    return -1;
+  }
+}
