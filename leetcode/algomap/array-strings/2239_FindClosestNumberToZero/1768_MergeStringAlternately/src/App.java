9339b03b4acac1323a5ddf493d154ca0d7cc9320
mayhrem <renecruz.0711@gmail.com>
2024-12-29 01:31:38 -0600

    Adding solution

--- leetcode/algomap/array-strings/2239_FindClosestNumberToZero/1768_MergeStringAlternately/src/App.java
+++ leetcode/algomap/array-strings/2239_FindClosestNumberToZero/1768_MergeStringAlternately/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        System.out.println(new Solution().mergeAlternately("abcd", "pqr")); // "apbqcrd"
+        System.out.println(new Solution().mergeAlternately("ab", "pqrs")); // "apbqrs"
+        System.out.println(new Solution().mergeAlternately("abcd", "pq"));
     }
 }
--- leetcode/algomap/array-strings/2239_FindClosestNumberToZero/1768_MergeStringAlternately/src/Solution.java
+++ leetcode/algomap/array-strings/2239_FindClosestNumberToZero/1768_MergeStringAlternately/src/Solution.java
@@ -1,3 +1,16 @@
 public class Solution {
-
+  public String mergeAlternately (String word1, String word2) {
+    int n1 = word1.length();
+    int n2 = word2.length();
+    StringBuilder result = new StringBuilder();
+    int p1 = 0;
+    int p2 = 0;
+    while (p1 < n1 || p2 < n2) {
+      if (p1 < n1) result.append(word1.charAt(p1));
+      if (p2 < n2) result.append(word2.charAt(p2));
+      p1++;
+      p2++;
+    }
+    return result.toString();
+  }
 }
