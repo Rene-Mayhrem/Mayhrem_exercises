1e39528a5f4428f5b2a75f7a3df97b651190cd9e
mayhrem <renecruz.0711@gmail.com>
2024-12-31 00:28:30 -0600

    Adding solution

---
+++ leetcode/algomap/hashmaps-sets/217_ContainsDuplicate/src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+
+public class Solution {
+  public boolean containsDuplicate (int[] nums) {
+    HashSet<Integer> values = new HashSet<>();
+    for (int num : nums) {
+      if (values.contains(num)) return true;
+      values.add(num);
+    }
+    return false; 
+  }
+}
