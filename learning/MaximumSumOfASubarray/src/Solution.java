39806ac3db4d864bd877a2034a2fb81478e312db
mayhrem <renecruz.0711@gmail.com>
2025-03-12 22:09:13 -0600

    Adding small project

---
+++ learning/MaximumSumOfASubarray/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+  // ? Problem statement: given an array of integers, find the maximum sum of a subarray of a fixed size "k"
+  public int maxSumSubarray (int[] nums, int k) {
+    int maxSum = 0;
+    int windowSum = 0;
+    
+    for (int i = 0; i < k; i++) {
+      
+    }
+  }
+}
