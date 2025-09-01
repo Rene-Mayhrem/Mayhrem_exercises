3443a328ac569e19f326420d93e1698ed8051272
mayhrem <renecruz.0711@gmail.com>
2025-08-31 19:52:33 -0600

    Adding new solutions

--- interview-prep/arrays-and-strings/two-pointers/125-valid-palindrome/Solution.java
+++ interview-prep/arrays-and-strings/two-pointers/125-valid-palindrome/Solution.java
@@ -1,5 +1,8 @@
 public class Solution {
     public boolean isPalindrome (String s) {
+        //? Removing all non-alphanumberical elements
+        s = s.replaceAll("^[A-Za-z0-9]", "").toLowerCase();
+        // This is O(n) space since we are creating a new and the strings are immutable
         //? Using pointers -> left and right
         int left = 0, right = s.length() - 1;
         //? Iterate the String s, while left <= right
@@ -13,4 +16,8 @@ public class Solution {
         //? return true after the while condition
         return true;
     }
+
+
 }
+
+
