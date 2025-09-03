7a41a74ea33c624bd4163edc2ca209cedcf88f0a
mayhrem <renecruz.0711@gmail.com>
2025-09-03 13:16:46 -0600

    Adding 283 exercise > completed

--- interview-prep/arrays-and-strings/two-pointers/283-move-zeroes/Solution.java
+++ interview-prep/arrays-and-strings/two-pointers/283-move-zeroes/Solution.java
@@ -1,3 +1,11 @@
 public class Solution {
-    
+        public void moveZeroes(int[] nums) {
+            
+            // Create two pointers -> one for numbers > 0 & other for zeros
+            // Use a loop to move throught the numbers in nums array
+                // Check if nums[i] is zero -> use loop to move that zero to the end of the line
+            
+
+        }
 }
+
