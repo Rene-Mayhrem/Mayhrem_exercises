1a0c00e693cef9ea5c480c62bb527dd482beb395
mayhrem <renecruz.0711@gmail.com>
2025-09-09 23:23:31 -0600

    Adding c-style reverse string exercise

---
+++ interview-prep/c-styiling/README.md
@@ -0,0 +1,3 @@
+Problem: Write an algorithm to reverse a C-style string.
+
+What do you think of when you hear "C-style string"? What does that mean for how we handle the string?
---
+++ interview-prep/c-styiling/Solution.java
@@ -0,0 +1,19 @@
+
+
+public class Solution {
+    public void reverseString (char[] characters) {
+        //? Create left and right pointers
+        int left = 0; // Initial char
+        int right = 0;
+        while (right < characters.length && characters[right] != '\0') right++;
+        right--;
+        //? Traverse the array
+        while (left < right || characters[left] == '\0') { 
+            char temp = characters[left];
+            characters[left] = characters[right];
+            characters[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
