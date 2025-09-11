42110922994d5d61e448ec9e45f5722c9b0a47a3
mayhrem <renecruz.0711@gmail.com>
2025-09-10 19:28:46 -0600

    Adding solution

--- interview-prep/217-contains-duplicate/Main.java
+++ interview-prep/217-contains-duplicate/Main.java
@@ -1,5 +1,18 @@
-package 217-contains-duplicate;
 
 public class Main {
     
+
+    public boolean containsDuplicate (String word) {
+        String formatedString = word.toLowerCase().trim();
+        if (formatedString.length() > 26) return true; 
+        boolean[] alphabet = {
+        false, false, false... false
+        };
+        for (int currentChar : formatedString.toCharArray()) {
+            int alphabetIndex = currentChar - 97; // since 'a' value is 97 in ascii
+            if (alphabet[alphabetIndex]) return true;
+            alphabet[alphabetIndex] = true;
+        }
+        return false;
+    }    
 }
