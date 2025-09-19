48e6ef21f126b52f125cce07c6846f0b4e0f84cb
mayhrem <renecruz.0711@gmail.com>
2025-09-18 22:24:29 -0600

    Solveed

---
+++ interview-prep/trees/BinaryTreeInorderTraversal/Solution.java
@@ -0,0 +1,37 @@
+package trees.BinaryTreeInorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public List<Integer> getInorderListTree(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<Integer> inorderList = new ArrayList<>();
+        Stack<TreeNode> helperStack = new Stack<>();
+        TreeNode current = root;
+        while (!helperStack.isEmpty() || current != null) {
+            //? Move all the elements to the left
+            while (current != null) {
+                helperStack.add(current);
+                current = current.left;    
+            }
+            TreeNode poppedNode = helperStack.pop();
+            inorderList.add(poppedNode.value);
+            if (poppedNode.right != null) current = poppedNode.right;
+        }
+        return inorderList;
+    }
+}
+
+
+class TreeNode {
+    int value;
+    TreeNode right;
+    TreeNode left;
+
+    TreeNode() {
+
+    }
+
+}
