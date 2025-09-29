6d1ca8fdbed102a75215f9492637b0b51f8b0610
mayhrem <renecruz.0711@gmail.com>
2025-09-28 21:41:01 -0600

    Java solution

--- interview-prep/ContainerWithMostWater/Main.java
+++ interview-prep/ContainerWithMostWater/Main.java
@@ -1,5 +1,20 @@
 package ContainerWithMostWater;
 
 public class Main {
-    
+    public int getContainerWithMostWater (int[] heights) {
+        int maxArea = 0;
+        int left = 0, right = heights.length - 1;
+        while (left < right) {
+            int base = right - left;
+            int width = Math.min(heights[left], heights[right]);
+            int currentArea = base * width;
+            maxArea = Math.max(currentArea, maxArea);
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
 }
